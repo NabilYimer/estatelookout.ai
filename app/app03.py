e20348e773b08c8e60dcaa5ad43391078c667603
Nabil Yimer <70453045+NabilYimer@users.noreply.github.com>
2024-04-12 09:05:08 +0300

    Update data path in app.py

--- app/app03.py
+++ app/app03.py
@@ -8,7 +8,7 @@ sys.path.append("/mount/src/estatelookout.ai/model")
 from Listing_Model import Listing_Model
 
 
-PROPERTY_DATA_FILE = "/mount/src/estatelookout.ai/data"
+PROPERTY_DATA_FILE = "/mount/src/estatelookout.ai/data/estate_data.csv"
 
 def load_listings(search_query: str, sale_type=None, num_beds=None):
     
